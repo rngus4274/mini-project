7aec27b6f9788dd5b8b7fd69b003178bfd1bae08
LEESEONGGWON <sysktho@gmail.com>
2020-09-09 13:12:55 +0900

    a

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -7,11 +7,11 @@ public class App {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("이성권");
-		System.out.println("안녕하세요");
-		System.out.println("11111");
 		System.out.println("3333");
 		int a;
 		a = 1;
+		System.out.println("11111");
+		System.out.println("이성권");
+		System.out.println("안녕하세요");
 	}
 }
