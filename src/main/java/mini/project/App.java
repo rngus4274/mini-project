2fc3f30a6aec9c6996555067aca32bd1a9f13295
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2020-09-09 13:09:11 +0900

    추가 수정

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -12,6 +12,12 @@ public class App {
 		System.out.println("11111");
 		System.out.println("3333");
 		int a;
+		
+		
+		System.out.println("테스트1");
+		System.out.println("테스트2");
+		System.out.println("테스트3");
+		
 		a = 1;
 	}
 }
